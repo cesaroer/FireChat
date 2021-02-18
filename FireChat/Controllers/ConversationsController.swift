d62c444748422e2adbdb3e92845226e2e8ace7a2
cesaroer <cesarvarcard@gmail.com>
2021-02-17 22:29:00 -0600

    Set the project elements and some lines of code

---
+++ FireChat/Controllers/ConversationsController.swift
@@ -0,0 +1,29 @@
+//
+//  ConversationsController.swift
+//  FireChat
+//
+//  Created by Cesar Rook on 17/02/21.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - lifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+    
+    //MARK: - Helpers
+
+    
+    
+}
---
+++ FireChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Cesar Rook on 17/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
