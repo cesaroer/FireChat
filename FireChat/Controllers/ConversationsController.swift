091362cb5f096f848ccbccaa91577d0b5f179e84
cesaroer <cesarvarcard@gmail.com>
2021-02-18 21:07:58 -0600

    Setting the table view style and confoguration

--- FireChat/Controllers/ConversationsController.swift
+++ FireChat/Controllers/ConversationsController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
     
     
     //MARK: - Properties
-    
+    private let tableView = UITableView()
     
     //MARK: - lifeCycle
     
@@ -33,9 +35,24 @@ class ConversationsController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         configureNavigationBar()
+        configureTableView()
 
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfie))
+    }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView() //Para los separadores
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+                           
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+        
     }
     
     func configureNavigationBar() {
@@ -46,7 +63,7 @@ class ConversationsController: UIViewController {
         appearance.backgroundColor = .systemPurple
         
         navigationController?.navigationBar.standardAppearance = appearance
-        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance //when make scroll up
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Messages"
@@ -59,7 +76,28 @@ class ConversationsController: UIViewController {
         navigationController?.navigationBar.barStyle = .black
     }
 
+}
+
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Im the cell # \(indexPath.row)"
+        return cell
+    }
     
     
 }
+
+extension ConversationsController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+
 
