4d4f08cc1724c8fc1d1f250a0736a6261dac8297
cesaroer <cesarvarcard@gmail.com>
2021-04-10 23:38:59 -0500

    Added ProfileViewModel and style for table view and table view cell

---
+++ FireChat/View/ProfileCell.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileCell.swift
+//  FireChat
+//
+//  Created by Cesar Rook on 10/04/21.
+//
+
+import Foundation
---
+++ FireChat/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  FireChat
+//
+//  Created by Cesar Rook on 10/04/21.
+//
+
+import Foundation
